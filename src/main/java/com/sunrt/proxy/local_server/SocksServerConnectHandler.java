8e3004174eb1d0f7ce8f0e6b407ed8d69212d8fc
SunMJin <1036524012@qq.com>
2019-11-08 17:31:18 +0800

    1

---
+++ src/main/java/com/sunrt/proxy/local_server/SocksServerConnectHandler.java
@@ -0,0 +1,183 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.sunrt.proxy.local_server;
+
+import com.sunrt.proxy.utils.SocksServerUtils;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.socks.SocksCmdRequest;
+import io.netty.handler.codec.socksx.SocksMessage;
+import io.netty.handler.codec.socksx.v4.DefaultSocks4CommandResponse;
+import io.netty.handler.codec.socksx.v4.Socks4CommandRequest;
+import io.netty.handler.codec.socksx.v4.Socks4CommandStatus;
+import io.netty.handler.codec.socksx.v5.*;
+import io.netty.util.CharsetUtil;
+import io.netty.util.NetUtil;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.FutureListener;
+import io.netty.util.concurrent.Promise;
+
+import java.util.Scanner;
+
+@ChannelHandler.Sharable
+public final class SocksServerConnectHandler extends SimpleChannelInboundHandler<SocksMessage> {
+
+    private final Bootstrap b = new Bootstrap();
+
+    @Override
+    public void channelRead0(final ChannelHandlerContext ctx, final SocksMessage message) throws Exception {
+        if (message instanceof Socks4CommandRequest) {
+            final Socks4CommandRequest request = (Socks4CommandRequest) message;
+            Promise<Channel> promise = ctx.executor().newPromise();
+            promise.addListener(
+                    new FutureListener<Channel>() {
+                        @Override
+                        public void operationComplete(final Future<Channel> future) throws Exception {
+                            final Channel outboundChannel = future.getNow();
+                            if (future.isSuccess()) {
+                                ChannelFuture responseFuture = ctx.channel().writeAndFlush(
+                                        new DefaultSocks4CommandResponse(Socks4CommandStatus.SUCCESS));
+
+                                responseFuture.addListener(new ChannelFutureListener() {
+                                    @Override
+                                    public void operationComplete(ChannelFuture channelFuture) {
+                                        ctx.pipeline().remove(SocksServerConnectHandler.this);
+                                        outboundChannel.pipeline().addLast(new RelayHandler(ctx.channel()));
+                                        ctx.pipeline().addLast(new RelayHandler(outboundChannel));
+                                    }
+                                });
+                            } else {
+                                ctx.channel().writeAndFlush(
+                                        new DefaultSocks4CommandResponse(Socks4CommandStatus.REJECTED_OR_FAILED));
+                                SocksServerUtils.closeOnFlush(ctx.channel());
+                            }
+                        }
+                    });
+
+            final Channel inboundChannel = ctx.channel();
+            b.group(inboundChannel.eventLoop())
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
+                    .option(ChannelOption.SO_KEEPALIVE, true)
+                    .handler(new DirectClientHandler(promise));
+
+            b.connect(request.dstAddr(), request.dstPort()).addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if (future.isSuccess()) {
+                        // Connection established use handler provided results
+                    } else {
+                        // Close the connection if the connection attempt has failed.
+                        ctx.channel().writeAndFlush(
+                                new DefaultSocks4CommandResponse(Socks4CommandStatus.REJECTED_OR_FAILED)
+                        );
+                        SocksServerUtils.closeOnFlush(ctx.channel());
+                    }
+                }
+            });
+        } else if (message instanceof Socks5CommandRequest) {
+            final Socks5CommandRequest request = (Socks5CommandRequest) message;
+            Promise<Channel> promise = ctx.executor().newPromise();
+            promise.addListener(
+                    new FutureListener<Channel>() {
+                        @Override
+                        public void operationComplete(final Future<Channel> future) throws Exception {
+                            final Channel outboundChannel = future.getNow();
+                            if (future.isSuccess()) {
+
+                                ChannelFuture responseFuture =
+                                        ctx.channel().writeAndFlush(new DefaultSocks5CommandResponse(
+                                                Socks5CommandStatus.SUCCESS,
+                                                request.dstAddrType(),
+                                                request.dstAddr(),
+                                                request.dstPort()));
+
+                                responseFuture.addListener(new ChannelFutureListener() {
+                                    @Override
+                                    public void operationComplete(ChannelFuture channelFuture) {
+                                        ctx.pipeline().remove(SocksServerConnectHandler.this);
+
+                                        sendCmdConnect(request,outboundChannel);
+
+                                        outboundChannel.pipeline().addLast(new RelayHandler(ctx.channel()));
+                                        ctx.pipeline().addLast(new RelayHandler(outboundChannel));
+                                    }
+                                });
+                            } else {
+                                ctx.channel().writeAndFlush(new DefaultSocks5CommandResponse(
+                                        Socks5CommandStatus.FAILURE, request.dstAddrType()));
+                                SocksServerUtils.closeOnFlush(ctx.channel());
+                            }
+                        }
+                    });
+
+            final Channel inboundChannel = ctx.channel();
+            b.group(inboundChannel.eventLoop())
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
+                    .option(ChannelOption.SO_KEEPALIVE, true)
+                    .handler(new DirectClientHandler(promise));
+
+            b.connect("127.0.0.1", 3080).addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if (future.isSuccess()) {
+                        // Connection established use handler provided results
+                    } else {
+                        // Close the connection if the connection attempt has failed.
+                        ctx.channel().writeAndFlush(
+                                new DefaultSocks5CommandResponse(Socks5CommandStatus.FAILURE, request.dstAddrType()));
+                        SocksServerUtils.closeOnFlush(ctx.channel());
+                    }
+                }
+            });
+        } else {
+            ctx.close();
+        }
+    }
+
+    public void sendCmdConnect(Socks5CommandRequest request,Channel outboundChannel){
+        ByteBuf byteBuf = Unpooled.directBuffer();
+        byteBuf.writeByte(request.version().byteValue());
+        byteBuf.writeByte(request.type().byteValue());
+        byteBuf.writeByte(0x00);
+        Socks5AddressType socks5AddressType=request.dstAddrType();
+        byteBuf.writeByte(request.dstAddrType().byteValue());
+        int port=request.dstPort();
+        String host=request.dstAddr();
+        if(request.dstAddrType().equals(Socks5AddressType.IPv4)){
+            byteBuf.writeBytes(NetUtil.createByteArrayFromIpAddressString(host));
+            byteBuf.writeShort(port);
+        }else if(socks5AddressType.equals(Socks5AddressType.DOMAIN)){
+            byteBuf.writeByte(host.length());
+            byteBuf.writeCharSequence(host, CharsetUtil.US_ASCII);
+            byteBuf.writeShort(port);
+        }else if(socks5AddressType.equals(Socks5AddressType.IPv6)){
+            byteBuf.writeBytes(NetUtil.createByteArrayFromIpAddressString(host));
+            byteBuf.writeShort(port);
+        }
+        outboundChannel.writeAndFlush(byteBuf);
+    }
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        SocksServerUtils.closeOnFlush(ctx.channel());
+    }
+}
---
+++ src/test/java/TimeClientHandler.java
@@ -0,0 +1,84 @@
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.socks.SocksAuthScheme;
+import io.netty.handler.codec.socks.SocksCmdRequest;
+import io.netty.handler.codec.socks.SocksInitRequest;
+import io.netty.handler.codec.socksx.v5.DefaultSocks5CommandRequest;
+import io.netty.handler.codec.socksx.v5.Socks5AddressType;
+import io.netty.handler.codec.socksx.v5.Socks5CommandRequest;
+import io.netty.handler.codec.socksx.v5.Socks5CommandType;
+import io.netty.util.CharsetUtil;
+import io.netty.util.NetUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * @author lilinfeng
+ * @date 2014年2月14日
+ * @version 1.0
+ */
+public class TimeClientHandler extends ChannelInboundHandlerAdapter {
+
+    private static final Logger logger = Logger
+            .getLogger(TimeClientHandler.class.getName());
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+
+
+        Socks5CommandRequest request=new DefaultSocks5CommandRequest(Socks5CommandType.CONNECT, Socks5AddressType.DOMAIN,"180.163.26.39",80);
+        ByteBuf byteBuf = Unpooled.directBuffer();
+        byteBuf.writeByte(request.version().byteValue());
+        byteBuf.writeByte(request.type().byteValue());
+        byteBuf.writeByte(0x00);
+        Socks5AddressType socks5AddressType=request.dstAddrType();
+        byteBuf.writeByte(request.dstAddrType().byteValue());
+        int port=request.dstPort();
+        String host=request.dstAddr();
+        if(request.dstAddrType().equals(Socks5AddressType.IPv4)){
+            byteBuf.writeBytes(NetUtil.createByteArrayFromIpAddressString(host));
+            byteBuf.writeShort(port);
+        }else if(socks5AddressType.equals(Socks5AddressType.DOMAIN)){
+            byteBuf.writeByte(host.length());
+            byteBuf.writeCharSequence(host, CharsetUtil.US_ASCII);
+            byteBuf.writeShort(port);
+        }else if(socks5AddressType.equals(Socks5AddressType.IPv6)){
+            byteBuf.writeBytes(NetUtil.createByteArrayFromIpAddressString(host));
+            byteBuf.writeShort(port);
+        }
+
+        List<SocksAuthScheme> list=new ArrayList<>();
+        list.add(SocksAuthScheme.NO_AUTH);
+        SocksInitRequest socksInitRequest=new SocksInitRequest(list);
+        ByteBuf buff = Unpooled.buffer();
+        socksInitRequest.encodeAsByteBuf(buff);
+
+        ctx.writeAndFlush(buff);
+
+
+        ctx.writeAndFlush(byteBuf);
+        System.out.println("发送了");
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg)
+            throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        System.out.println(Arrays.toString(req));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // 释放资源
+        logger.warning("Unexpected exception from downstream : "
+                + cause.getMessage());
+        ctx.close();
+    }
+}
